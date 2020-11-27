3066165910ea7291b6c6e02b7f0e125c12f23f54
Anthony Bradley <anthonybradley@Inteross-MacBook-Pro.local>
2020-11-27 09:32:16 -0500

    fix cabal version so using proper mixins

--- Main.hs
+++ Main.hs
@@ -1,9 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Web.Scotty
 
-import Data.Monoid (mconcat)
-
-main = scotty 3000 $
-    get "/:word" $ do
-        beam <- param "word"
-        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+main :: IO ()
+main =
+  print "Hello World"
